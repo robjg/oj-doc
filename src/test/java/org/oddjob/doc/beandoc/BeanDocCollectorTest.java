895ac202be5c1824b08e05b7f726bfaf2c59c796
Rob Gordon <rob@rgordon.co.uk>
2026-01-19 18:00:15 +0000

    Use a conversion doc strategy to support ValueFactory conversions.

--- src/test/java/org/oddjob/doc/beandoc/BeanDocCollectorTest.java
+++ src/test/java/org/oddjob/doc/beandoc/BeanDocCollectorTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.oddjob.arooa.beandocs.WriteableConversionDoc;
 import org.oddjob.arooa.beandocs.WriteableConversionDocs;
 import org.oddjob.arooa.convert.ClassOrMethod;
+import org.oddjob.arooa.convert.doc.ConversionItemAccess;
 import org.oddjob.doc.doclet.Conversions;
 
 import java.lang.reflect.Method;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BeanDocCollectorTest {
 
@@ -25,10 +28,15 @@ class BeanDocCollectorTest {
         conversionDoc.setTypeOrMethod(ClassOrMethod.ofMethod(method).getName());
         conversionDoc.setFromType(Number.class.getTypeName());
 
-        WriteableConversionDocs conversionsByType = new WriteableConversionDocs();
-        conversionsByType.add(ClassOrMethod.ofMethod(method), conversionDoc);
+        ConversionItemAccess<WriteableConversionDoc> itemAccess = mock(ConversionItemAccess.class);
+        when(itemAccess.containsForType(BeanDocCollectorTest.class.getCanonicalName()))
+                .thenReturn(true);
+        when(itemAccess.getForMethod(BeanDocCollectorTest.class.getCanonicalName(), methodName))
+                .thenReturn(conversionDoc);
 
-        Conversions conversions = new Conversions(conversionsByType);
+        WriteableConversionDocs conversionsDocs = new WriteableConversionDocs(itemAccess);
+
+        Conversions conversions = new Conversions(conversionsDocs);
 
         Conversions.As as = conversions.docByType(BeanDocCollectorTest.class.getTypeName());
 
